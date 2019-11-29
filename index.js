25aeb29e27adc052eb7c8051c47f0b604e928bfc
adoniramj <adoniramjvargas@gmail.com>
2019-11-29 17:33:27 -0500

    index.js

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
-// const server = http.createServer((req,res) => {
-//   if (req.url === '/'){
-//     fs.readFile(
-//       path.join(__dirname, 'public', 'index.html'), 
-//       (err, content) => {
-//       if (err) throw err
-//       res.writeHead(200, { 'Content-type': 'text/html'})
-//       res.end(content)
-//     })
-//   }
-// })
-
 const server = http.createServer((req, res) => {
   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
   console.log(filePath)
